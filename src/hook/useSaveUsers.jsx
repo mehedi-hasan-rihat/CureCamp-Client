7f7b949b1b3dc72c85c09a69271ed920e2d3fdd0
mehedi-15 <mehedi4794@gmail.com>
2025-01-24 16:43:58 +0600

    remove axiosSecure and and axios in saveUSer

--- src/hook/useSaveUsers.jsx
+++ src/hook/useSaveUsers.jsx
@@ -11,8 +11,7 @@ console.log(4);
             photoURL: user?.photoURL,
         };
         try {
-            // const { data } = await axios.post(`${import.meta.env.VITE_URL}/users`, userData);
-            const { data } = await axiosSecure.post(`/users`, userData);
+            const { data } = await axios.post(`${import.meta.env.VITE_URL}/users`, userData);
             console.log('User saved:', data);
             return data;
         } catch (error) {
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -145,8 +145,8 @@ export default function SignUp() {
 
 
                      const data = await signInWithGoogle()
-                      console.log(data.user);
-                      console.log(loading);
+                      // console.log(data.user);
+                      // console.log(loading);
                       saveUser(data.user).then(res => navigate('/'))
                     }}
                     className="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500"
